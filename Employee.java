d2322576fc9d7ae72a3684e208d7240c777eb1ed
pawan singh <kushpawal1@gmail.com>
2021-04-25 20:31:44 +0530

    [Pawan] added the welcom line in Employee.java

---
+++ Employee.java
@@ -0,0 +1,8 @@
+class Employee
+{
+        public static void main(String [] args)
+        {
+		//master added this walcome line..
+                System.out.println("Welcome to Employee Wage Computation Program");
+        }
+}
