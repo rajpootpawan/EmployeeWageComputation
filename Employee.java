e5b9757eb4a4c6bd39dfe591864bf580ba930b14
pawan singh <kushpawal1@gmail.com>
2021-04-25 21:51:25 +0530

    [user6] added whicle conditon to calculate total wage

--- Employee.java
+++ Employee.java
@@ -7,25 +7,39 @@ class Employee
                 System.out.println("Welcome to Employee Wage Computation Program");
 
 		//User1 Added the Random Function..
+		 //User 6 aadded a while condition to calculate total wage..
+                int hour=0;
+                int days=0;
+                int totalWage=0;
+                while(hour<100 || days<20)
+                {
+
 		Random r= new Random();
 		int num=r.nextInt(2);
 		//User 4 added the switch Case statements
-		switch(num){
-		case 0:
-			System.out.println("Employee Absent");
-			break;
-		case 1:
-			System.out.println("Employee Present");
-			break;
+		  switch(num){
+                case 0:
+                        System.out.println("Employee Absent");
+                        break;
+                case 1:
 
-
+                        System.out.println("Employee Present");
+                        totalWage=totalWage+oneDayWage();
+                        days++;
+                        hour=hour+20;
+                        break;
 		}
+	}
 		 //User 2 Added function to calculate the one Day Wage..
                 System.out.println("OneDay Wage is "+oneDayWage());
 		//User 3 added Function to calculte one day wage After add parttime..
 		System.out.println("Wage After added part time "+partTimeWage());
 		 //User 5 added a function to calculate wage for a month..
                 System.out.println("Wage For month "+wageMonth());
+		 if(hour==100)
+                        System.out.println("Total Wage for 100 hours :"+totalWage);
+                else
+                        System.out.println("Total wage For 20 Days  :"+totalWage);
 
 
 
